c3dd0c627702e5a566da61c699b19c514c40595c
Andrei Lespukh <alespukh@evolutiongaming.com>
2017-09-21 14:33:40 +0300

    iintermidiate

--- modules/shapeless/src/main/io/parsek/shapeless/HListDecoder.scala
+++ modules/shapeless/src/main/io/parsek/shapeless/HListDecoder.scala
@@ -1,9 +1,12 @@
-import io.parsek.Decoder
+import io.parsek.{Decoder, PValue}
 
 
 trait HListDecoders {
   import shapeless.HList
-  implicit def hlistDecoder[A: Decoder, +T <: HList : Decoder]:Decoder[A :: T]
+  import shapeless.::
+  implicit def hlistDecoder[A: Decoder, +T <: HList : Decoder]:Decoder[A :: T] = new Decoder[A:: T]{
+    override def apply(v: PValue) = ???
+  }
 
   implicit def hnillDecoder: Decoder[HNil]
 }
