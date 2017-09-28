c8ec61db1d6f32dc1c0f85bfdb8c1b03b9c8ec40
Andrei Tupitcyn <andrew.tupitsin@gmail.com>
2017-09-28 12:52:29 +0300

    feat: extend PValueFormatted with formatInt, formatLong, formatDouble

--- modules/core/src/main/scala/io/parsek/PValueFormatter.scala
+++ modules/core/src/main/scala/io/parsek/PValueFormatter.scala
@@ -7,28 +7,26 @@ import java.time.temporal.{TemporalAccessor, TemporalQuery}
 /**
   * @author andr83
   */
-trait PValueFormatter {
-  def formatInstant(time: Instant): Long Either String
+class PValueFormatter {
+  def formatInstant(time: Instant): Long Either String = Left(time.toEpochMilli)
 
-  def formatDate(date: LocalDate): String
+  def formatDate(date: LocalDate): String = DateTimeFormatter.ISO_LOCAL_DATE.format(date)
 
-  def parseInstant(value: String): Instant
+  def formatDouble(value: Double): Double = value
+
+  def formatInt(value: Int): Int = value
+
+  def formatLong(value: Long): Long = value
+
+  def parseInstant(value: String): Instant = Instant.parse(value)
 
   def parseInstant(value: Long): Instant = Instant.ofEpochMilli(value)
 
-  def parseDate(value: String): LocalDate
+  def parseDate(value: String): LocalDate = LocalDate.from(DateTimeFormatter.ISO_LOCAL_DATE.parse(value))
 }
 
 object PValueFormatter {
-  def apply(): PValueFormatter = new PValueFormatter {
-    override def formatInstant(time: Instant): Either[Long, String] = Left(time.toEpochMilli)
-
-    override def parseInstant(value: String): Instant = Instant.parse(value)
-
-    override def formatDate(date: LocalDate): String = DateTimeFormatter.ISO_LOCAL_DATE.format(date)
-
-    override def parseDate(value: String): LocalDate = LocalDate.from(DateTimeFormatter.ISO_LOCAL_DATE.parse(value))
-  }
+  def apply(): PValueFormatter = new PValueFormatter
 
   def apply(dateTimePattern: String, datePattern: String): PValueFormatter =
     apply(DateTimeFormatter.ofPattern(dateTimePattern).withZone(ZoneId.systemDefault()),
