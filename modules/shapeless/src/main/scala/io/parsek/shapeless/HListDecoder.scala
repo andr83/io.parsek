50393e950fe25ad6be8f7fa30e46fde095046dc2
Bulat Fattahov <bfattahov@evolutiongaming.com>
2017-10-04 01:20:09 +0300

    Made Decoder configurable. Refactored and cleaned.

--- modules/shapeless/src/main/scala/io/parsek/shapeless/HListDecoder.scala
+++ modules/shapeless/src/main/scala/io/parsek/shapeless/HListDecoder.scala
@@ -1,4 +1,4 @@
-package io.parsek.shapeless.weak
+package io.parsek.shapeless
 
 import io.parsek.Decoder.Result
 import io.parsek.PValue.{PMap, PNull}
@@ -16,37 +16,50 @@ trait HListDecoder {
                                                                            implicit
                                                                            witness: Witness.Aux[K],
                                                                            hDecoder: Lazy[DecoderWithDefaults[H]],
-                                                                           tDecoder: Lazy[DecoderWithDefaults[T]]
+                                                                           tDecoder: Lazy[DecoderWithDefaults[T]],
+                                                                           decodeConfiguration: Configuration
                                                                          ): DecoderWithDefaults[FieldType[K, H] :: T] =
     new DecoderWithDefaults[FieldType[K, H] :: T] {
       override def apply(v: PValue, defaults: Map[Symbol, PValue]): Result[FieldType[K, H] :: T] = {
-        val headResult: Decoder.Result[FieldType[K, H]] = {
+        val headResult: Decoder.Result[(PValue, FieldType[K, H])] = {
           v match {
             case p@PMap(valuesMap) =>
-              val fieldName = witness.value
+              val fieldName = decodeConfiguration.lookUpFieldName(witness.value)
               val newPMap = PMap(valuesMap - fieldName)
               valuesMap
                 .get(fieldName)
-                .orElse(defaults.get(fieldName))
-                .orElse(Some(PNull)) // we try to decode object from PNull, it's possible, for instance, for Option[T]
+                .orElse(if (decodeConfiguration.useDefaults) defaults.get(fieldName) else None)
+                .orElse(if (decodeConfiguration.tryPNullForEmptyFields) Some(PNull) else None)
                 .map(pv => hDecoder.value(pv, defaults)
-                .right.map(shapeless.labelled.field[K].apply _))
+                  .right.map(shapeless.labelled.field[K].apply _)
+                  .right.map(newPMap -> _))
                 .getOrElse(Left(new IllegalArgumentException(s"Field $fieldName does not exist in PMap")))
             case _ => Left(new IllegalArgumentException("Case classes have to map to PMap"))
           }
         }
 
-        def buildTail = tDecoder.value.apply(v, defaults)
+        def buildTail(newPValue: PValue) = tDecoder.value.apply(newPValue, defaults)
 
-        for {
-          head <- headResult.right
-          tail <- buildTail.right
-        } yield head :: tail
+        for (
+          head <- headResult.right;
+          tail <- buildTail(head._1).right
+        ) yield head._2 :: tail
       }
     }
 
-  implicit def hNilDecoderWithDefaults: DecoderWithDefaults[HNil] = new DecoderWithDefaults[HNil] {
-    override def apply(v: PValue, defaults: Map[Symbol, PValue]): Result[HNil] = Right(HNil)
+  implicit def hNilDecoderWithDefaults(implicit decodeConfiguration: Configuration): DecoderWithDefaults[HNil] = new DecoderWithDefaults[HNil] {
+    override def apply(v: PValue, defaults: Map[Symbol, PValue]): Result[HNil] =
+      if (decodeConfiguration.allowAdditionalFields)
+        Right(HNil)
+      else
+        v match {
+          case PNull => Right(HNil)
+          case PMap(map) if map.isEmpty => Right(HNil)
+          case x => Left(new IllegalArgumentException(
+            s"Pvalue is not fully converted to case class. " +
+              s"Check your data or use io.parsek.shapeless.weak.HListDecoder to avoid this error. " +
+              s"Rest part is $x"))
+        }
   }
 
   implicit def decoderToWithDefault[T](implicit d: Decoder[T]): DecoderWithDefaults[T] = new DecoderWithDefaults[T] {
@@ -57,7 +70,8 @@ trait HListDecoder {
                                                          gen: LabelledGeneric.Aux[H, T],
                                                          td: Lazy[DecoderWithDefaults[T]],
                                                          defaults: Default.AsRecord.Aux[H, D],
-                                                         defaultEncoder: Lazy[Encoder[D]]): Decoder[H] =
+                                                         defaultEncoder: Lazy[Encoder[D]],
+                                                         decodeConfiguration: Configuration): Decoder[H] =
     new Decoder[H] {
       def apply(pValue: PValue): Decoder.Result[H] = {
         val defaultPValue: Map[Symbol, PValue] =
@@ -73,5 +87,5 @@ trait HListDecoder {
 
 }
 
-object HListDecoder extends HListDecoder
+object HListDecoder extends HListDecoder with Configuration.Weak
 
--- modules/shapeless/src/test/scala/io/parsek/shapeless/HListDecoderSpec.scala
+++ modules/shapeless/src/test/scala/io/parsek/shapeless/HListDecoderSpec.scala
@@ -1,19 +1,24 @@
-package io.parsek.shapeless.weak
+package io.parsek.shapeless
 
-import cats.data.Ior
 import io.parsek.Decoder.decode
-import io.parsek.{Decoder, PValue}
+import io.parsek.PValue
 import io.parsek.PValue._
-import org.scalatest.{FunSuite, Matchers, WordSpec}
+import io.parsek.shapeless.weak.HListDecoder
+import org.scalatest.{Matchers, WordSpec}
 
 /**
   * Created by bfattahov on 03/10/2017.
   */
 class HListDecoderSpec extends WordSpec with Matchers {
 
-  "Weak HListDecoder" should {
+  "HListDecoder" should {
+
+    import io.parsek.shapeless.HListDecoder._
+    import io.parsek.shapeless.HListEncoder._
+    import io.parsek.instances.DecoderInstances._
+    import io.parsek.instances.EncoderInstances._
+
     "break decoding of an Option[T]" in {
-      import io.parsek.shapeless.weak.HListDecoder._
       import io.parsek.instances.DecoderInstances._
       decode[Option[Int]](PInt(3)) shouldEqual Right(Some(3))
       decode[Option[Int]](PNull) shouldEqual Right(None)
@@ -21,10 +26,6 @@ class HListDecoderSpec extends WordSpec with Matchers {
     }
 
     "decode case classes" in {
-      import io.parsek.shapeless.weak.HListDecoder._
-      import io.parsek.shapeless.HListEncoder._
-      import io.parsek.instances.DecoderInstances._
-      import io.parsek.instances.EncoderInstances._
       case class Address(street: String, city: String, building: Int, flat: Int)
       //recently it does not work with recursive types
       case class User(surname: String, name: String, address: Address /*, spouse: Option[User] = None*/)
@@ -60,43 +61,51 @@ class HListDecoderSpec extends WordSpec with Matchers {
     }
 
     "decode case classes with defaults" in {
-      import HListDecoder._
-      import io.parsek.instances.DecoderInstances._
-      import io.parsek.shapeless.HListEncoder._
-      import io.parsek.instances.EncoderInstances._
-
       case class A(name: String, b: Int = 4)
 
       decode[A](PValue.pmap('name -> PString("aname"), 'b -> PInt(5))) shouldEqual Right(A("aname", 5))
       decode[A](PValue.pmap('name -> PString("aname"))) shouldEqual Right(A("aname", b = 4))
-      decode[A](PValue.pmap('name344 -> PString("aname"))) should be (a[Left[_, _]])
+      decode[A](PValue.pmap('name344 -> PString("aname"))) should be(a[Left[_, _]])
     }
 
     "decode case classes with Options" in {
-      import HListDecoder._
-      import io.parsek.instances.DecoderInstances._
-      import io.parsek.shapeless.HListEncoder._
-      import io.parsek.instances.EncoderInstances._
       case class B(name: String, b: Option[Int] = None)
       decode[B](PValue.pmap('name -> PString("aname"), 'b -> PInt(5))) shouldEqual Right(B("aname", Some(5)))
       decode[B](PValue.pmap('name -> PString("aname"))) shouldEqual Right(B("aname", None))
     }
+  }
 
-    "Work with non-matching PValues" in {
-      import HListDecoder._
-      import io.parsek.instances.DecoderInstances._
-      import io.parsek.shapeless.HListEncoder._
-      import io.parsek.instances.EncoderInstances._
+  "HListDecoder" should {
+
+    import io.parsek.shapeless.HListEncoder._
+    import io.parsek.instances.DecoderInstances._
+    import io.parsek.instances.EncoderInstances._
+
+    "fail in non-matching PValues in strict Configuration" in {
+      implicit val hd = new HListDecoder with Configuration.Strict
+      import hd._
 
       case class C(f1: String)
-      decode[C](PValue.pmap('f1-> PString("f1"), 'unexpected -> PInt(5))) should be (Right(C("f1")))
+      decode[C](PValue.pmap('f1 -> PString("f1"), 'unexpected -> PInt(5))) should be(a[Left[_, _]])
 
-      case class D(a:String, b:Option[Int])
+      case class D(a: String, b: Option[Int])
 
-      decode[D](PValue.pmap('a-> PString("a"))) should be (Right(D("a", None)))
-      decode[D](PValue.pmap('a-> PString("a"), 'b -> PInt(45))) should be (Right(D("a", Some(45))))
+      decode[D](PValue.pmap('a -> PString("a"))) should be(a[Left[_, _]])
+      decode[D](PValue.pmap('a -> PString("a"), 'b -> PInt(45))) should be(Right(D("a", Some(45))))
     }
 
+    "Work with non-matching PValues in weak Configuration" in {
+      implicit val hd = new HListDecoder with Configuration.Weak
+      import hd._
+
+      case class C(f1: String)
+      decode[C](PValue.pmap('f1 -> PString("f1"), 'unexpected -> PInt(5))) should be(Right(C("f1")))
+
+      case class D(a: String, b: Option[Int])
+
+      decode[D](PValue.pmap('a -> PString("a"))) should be(Right(D("a", None)))
+      decode[D](PValue.pmap('a -> PString("a"), 'b -> PInt(45))) should be(Right(D("a", Some(45))))
+    }
   }
 
 }
